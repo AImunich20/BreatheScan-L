90e033426c4a5b7521593c69985a37aa576d71e5
AI_intelligence_genZ <natthanathron@gmail.com>
2025-12-24 18:46:17 +0700

    2

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ def test():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=2020)
